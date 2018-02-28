f9bd58ef6f8be7bfcf38a193daf115dfd8d134c2
tlburris3 <tlburris3@yahoo.com>
2018-02-28 15:08:43 -0600

    Starting JavaScript portion of course.

---
+++ JavaScript/basicsOfJSScript.js
@@ -0,0 +1,5 @@
+
+
+var userName = prompt("Hi! Welcome to the introduction of JS. What is your name?")
+alert("Nice to meet you, " + userName + "!")
+console.log("Also great to meet you " + userName);
