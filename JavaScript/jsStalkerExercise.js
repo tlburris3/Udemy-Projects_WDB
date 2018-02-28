d630c463c0de90f9339506add412a1c8fa2c5192
tlburris3 <tlburris3@yahoo.com>
2018-02-28 15:22:12 -0600

    created another variable for simplicity.

--- JavaScript/jsStalkerExercise.js
+++ JavaScript/jsStalkerExercise.js
@@ -1,9 +1,10 @@
 var firstName = prompt("What is your first name?");
 var lastName = prompt("What about your last name?");
 var userAge = prompt("Last question, what's your age?");
+var fullName = firstName + " " + lastName;
 
-alert("Your full name is " + firstName + " " + lastName)
+alert("Your full name is " + fullName)
 alert("You are " + userAge + " years old")
 
-console.log("Your full name is " + firstName + " " + lastName)
+console.log("Your full name is " + fullName)
 console.log("You are " + userAge + " years old")
