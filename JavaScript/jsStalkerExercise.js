ede818daa26eee065fe290dee505945eca28f990
tlburris3 <tlburris3@yahoo.com>
2018-02-28 15:19:42 -0600

    Little exercise for name prompts from user.

---
+++ JavaScript/jsStalkerExercise.js
@@ -0,0 +1,9 @@
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What about your last name?");
+var userAge = prompt("Last question, what's your age?");
+
+alert("Your full name is " + firstName + " " + lastName)
+alert("You are " + userAge + " years old")
+
+console.log("Your full name is " + firstName + " " + lastName)
+console.log("You are " + userAge + " years old")
