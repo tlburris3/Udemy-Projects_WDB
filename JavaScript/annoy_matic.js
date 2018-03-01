644d7c2220d905aa873545de9c5687f29b11096e
tlburris3 <tlburris3@yahoo.com>
2018-03-01 15:34:36 -0600

    Found something new about the indexOf() function in JS, so we were asked to use it.

--- JavaScript/annoy_matic.js
+++ JavaScript/annoy_matic.js
@@ -1,8 +1,14 @@
 var answer = prompt("Are we there yet?");
 
-while (answer !== "yes" && answer !== "yeah")
-{
-	var answer = prompt("Are we there yet?");	
-}
+// while (answer !== "yes" && answer !== "yeah")
+// {
+// 	var answer = prompt("Are we there yet?");	
+// }
 
+// Note: indexOf() function looks within the sentence for that specific
+// string that you have inside the parenthesis.
+while (answer.indexOf("yes") === -1 && answer.indexOf("yeah") === -1)
+{
+	var answer = prompt("Are we there yet?");
+}
 alert("YEAH BOY, WE HERE!")
