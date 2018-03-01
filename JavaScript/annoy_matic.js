74fdb6be034ac8423ec8aed432a5edec33dd91e9
tlburris3 <tlburris3@yahoo.com>
2018-03-01 15:28:17 -0600

    making a while loop exercise within JS to test things out.

---
+++ JavaScript/annoy_matic.js
@@ -0,0 +1,8 @@
+var answer = prompt("Are we there yet?");
+
+while (answer !== "yes" && answer !== "yeah")
+{
+	var answer = prompt("Are we there yet?");	
+}
+
+alert("YEAH BOY, WE HERE!")
--- JavaScript/jsGuessingGameExercise.js
+++ JavaScript/jsGuessingGameExercise.js
@@ -7,10 +7,17 @@ if (userNumber === null) {
 }
 else if (userNumber > answer) {
 	alert("Your number is too high!")
+	console.log("users num was " + userNumber)
+
 }
 else if (userNumber < answer) {
 	alert("Your number is too low!")
+	console.log("users num was " + userNumber)
+
 }
 else {
 	alert("Good guess, you got the number!")
+	console.log("users num was " + userNumber)
 }
+// Note: You can turn a string into a number by using
+// the Number() function.
