c98dcb596d78fb23e4da7fc94cece05aae31d593
tlburris3 <tlburris3@yahoo.com>
2018-03-05 18:52:51 -0600

    Part 2 of the todo list - Added some more functionality and modularity to the JS section of the todo list.

--- JavaScript/jsTodoList.js
+++ JavaScript/jsTodoList.js
@@ -1,19 +1,49 @@
+// ToDo List - Part 1
 var todos = [];
 
-var input = prompt("What would you like to do?");
+window.setTimeout(function(){}, 7000);
 
+var input = prompt("What would you like to do?");
 while (input !== "exit") {
 	if (input === "list") {
-		console.log(todos)
+		listTodos();
 	}
 	else if (input === "new") {
-		// ask for new todo
-		var newTodo = prompt("Enter new todo");
-		// add to todos array
-		todos.push(newTodo);
+		newTodos();
+	}
+	else if (input === "delete") {
+		deleteTodos();
 	}
 
+	// Ask again for another function.
 	input = prompt("What would you like to do?");
 }
 
-console.log("Ok, you just exited the app.")
+console.log("Ok, you just exited the app.");
+
+// Modularity
+function listTodos() {
+	console.log("**********");
+	// Note: "i" naturally becomes a counter when used as a param
+	// inside the anonymous function.
+	todos.forEach(function (item, i) {
+		console.log(i + ": " + item);
+	});
+	console.log("**********");
+}
+
+function newTodos() {
+	// ask for new todo
+	var newTodo = prompt("Enter new todo");
+	// add to todos array
+	todos.push(newTodo);
+	console.log(newTodo + " added to list.");
+}
+
+function deleteTodos() {
+	var index = prompt("Enter an index of the list to delete");
+	// Note: splice - index to remove at, how many values from that index.
+	var deletedItem = todos.splice(index, 1);
+	console.log("Deleted todo " + deletedItem);
+}
+
