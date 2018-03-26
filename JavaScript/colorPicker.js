5d56cea9013810366759ff02c91bd9f4499be240
tlburris3 <tlburris3@yahoo.com>
2018-03-26 15:23:54 -0500

    Created an DOM manipulation .js file, which contains functions that will change the background of a webpage to a random index (that is generated from a generator function) within an array of colors.

---
+++ JavaScript/colorPicker.js
@@ -0,0 +1,45 @@
+/* colorPicker.js
+ * 
+ * Purpose:
+ * 		For better understanding how to implement
+ * 		the addEventListener(type, functionToCall);
+ * 		JS function.
+ * Notes:
+ * 		Every time the button is clicked,
+ * 		a new color from the array will be randomly
+ * 		chosen, based off a random index generator
+ * 		function. The background will then be changed
+ * 		to the chosen color.
+ */
+
+var colors = ["green", "red", "blue", "purple", "orange"];
+
+var button = document.querySelector("button");
+var body = document.querySelector("body");
+
+button.addEventListener("click", changeColor);
+
+/* randomIndex
+ *
+ * Purpose:
+ * 		Generates a random number used for an index.
+ * Returns:
+ * 		Random index number between 0 and 5 (exclusive).
+ */
+function randomIndex() {
+	return Math.floor(Math.random() * colors.length);
+}
+
+/* randomIndex
+ *
+ * Purpose:
+ * 		Changes the background color of the body element.
+ * Notes:
+ * 		Uses the randomIndex() function to choose the
+ * 		color which will it will be changed to.
+ */
+function changeColor() {
+	var color = colors[randomIndex()];
+	body.style.background = color;
+	console.log("color is: " + color);
+}
