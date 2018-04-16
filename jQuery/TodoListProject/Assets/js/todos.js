02c7891c27be37734e78ccb7889790d6d43bc785
tlburris3 <tlburris3@yahoo.com>
2018-04-16 12:40:03 -0500

    Manipulating the span in the li to fade out and delete after .5 seconds. Also learned how to stop "event bubbling" from happening so the rest of the functionality from the li won't occur at the same time as the span's functionality.

--- jQuery/TodoListProject/Assets/js/todos.js
+++ jQuery/TodoListProject/Assets/js/todos.js
@@ -1,17 +1,11 @@
 // check off specific todos by clicking
 $("li").click(function() {
-	// if black - turn gray
-	if ($(this).css("color") === "rgb(128, 128, 128)") {
-		$(this).css({
-			color: "black",
-			textDecoration: "none"
-		});
-	}
-	else {
-		$(this).css({
-			color: "gray",
-			textDecoration: "line-through"
-		});
-	}
-	// if gray - turn black
+	$(this).toggleClass("completed");
+});
+
+$("span").click(function(event) {
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
 });
