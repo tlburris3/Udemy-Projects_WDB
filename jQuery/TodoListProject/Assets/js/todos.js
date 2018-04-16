9258ac89bd5377c3dc2ce3cc79b3475d5e3b2df5
tlburris3 <tlburris3@yahoo.com>
2018-04-16 18:01:03 -0500

    Finished Todo List project.

--- jQuery/TodoListProject/Assets/js/todos.js
+++ jQuery/TodoListProject/Assets/js/todos.js
@@ -1,11 +1,40 @@
-// check off specific todos by clicking
-$("li").click(function() {
+// Check off specific todos by clicking.
+$("ul").on("click", "li", function() {
 	$(this).toggleClass("completed");
 });
 
-$("span").click(function(event) {
+// Delete specific todos from span.
+$("ul").on("click", "span", function(event) {
 	$(this).parent().fadeOut(500, function(){
 		$(this).remove();
 	});
 	event.stopPropagation();
 });
+
+// Adding user input as a new todo in list.
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13) {
+		var inputText = $(this).val();
+		$(this).val("");
+		addTodo(inputText);
+	}
+});
+
+// Fading in and out the Add New Todo
+$(".fa-pencil").click(function() {
+	$("input[type='text']").fadeToggle();
+});
+
+/**
+  * addTodo
+  *
+  * Purpose:
+  * 	Takes the user input and appends it
+  * 	to the todo list.
+  * Parameters:
+  * 	event: 	User's input.
+  **/
+function addTodo(event) {
+	$("ul").append("<li><i class='fa fa-trash'></i> " +
+		event + "</li>");
+}
